4e04f99ed30685907f7887eae7cdf83c41ac0523
tiawl <pabtomas@protonmail.com>
2024-02-12 20:36:19 +0100

    [update #0] build.zig

--- build.zig
+++ build.zig
@@ -6,10 +6,10 @@ pub fn build (builder: *std.Build) void
   const optimize = builder.standardOptimizeOption (.{});
 
   const exe = builder.addExecutable (.{
-      .name = "test",
-      .root_source_file = .{ .path = "src/main.zig" },
-      .target = target,
-      .optimize = optimize,
+    .name = "generator",
+    .root_source_file = .{ .path = "src/main.zig" },
+    .target = target,
+    .optimize = optimize,
   });
 
   builder.installArtifact (exe);
@@ -19,4 +19,34 @@ pub fn build (builder: *std.Build) void
 
   const run_step = builder.step ("run", "Run the app");
   run_step.dependOn (&run_cmd.step);
+
+  const lib = builder.addStaticLibrary (.{
+    .name = "cimgui",
+    .target = target,
+    .optimize = optimize,
+  });
+
+  lib.addIncludePath (.{ .path = "imgui" });
+  lib.addIncludePath (.{ .path = "imgui/backends" });
+
+  lib.addCSourceFiles (.{
+    .files = &.{
+      "cimgui.cpp",
+      "cimgui_impl_glfw.cpp",
+      "cimgui_impl_vulkan.cpp",
+      "imgui/imgui.cpp",
+      "imgui/imgui_demo.cpp",
+      "imgui/imgui_draw.cpp",
+      "imgui/imgui_tables.cpp",
+      "imgui/imgui_widgets.cpp",
+      "imgui/backends/imgui_impl_glfw.cpp",
+      "imgui/backends/imgui_impl_vulkan.cpp",
+    },
+  });
+  lib.linkLibC ();
+  lib.installHeadersDirectory ("imgui", "imgui");
+  lib.installHeader ("cimgui.h", "cimgui.h");
+  lib.installHeader ("cimgui_impl_glfw.h", "cimgui_impl_glfw.h");
+  lib.installHeader ("cimgui_impl_vulkan.h", "cimgui_impl_vulkan.h");
+  builder.installArtifact (lib);
 }
