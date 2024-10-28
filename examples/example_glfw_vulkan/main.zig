0825e4bc6a0dba2694d9a5130edefcd6381227a3
tiawl <pabtomas@protonmail.com>
2024-10-28 09:23:14 +0100

    [update] example to new dear_bindings conventions

--- examples/example_glfw_vulkan/main.zig
+++ examples/example_glfw_vulkan/main.zig
@@ -5,7 +5,7 @@ pub const c = @cImport ({
   @cDefine ("GLFW_INCLUDE_VULKAN", "1");
   @cDefine ("GLFW_INCLUDE_NONE", "1");
   @cInclude ("GLFW/glfw3.h");
-  @cInclude ("cimgui.h");
+  @cInclude ("dcimgui.h");
   @cInclude ("backends/cimgui_impl_glfw.h");
   @cInclude ("backends/cimgui_impl_vulkan.h");
 });
