674f6086ef5c3f64eb95f79c747b6feec35dc34c
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-01-27 22:13:03 +0100

    Added simple GLFW/OpenGL3 example

---
+++ examples/example_glfw_opengl3/main.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+
+const c = @cImport({
+    @cInclude("GLFW/glfw3.h");
+    @cInclude("dcimgui.h");
+    @cInclude("backends/cimgui_impl_glfw.h");
+    @cInclude("backends/cimgui_impl_opengl3.h");
+});
+
+fn errorCallback(errn: c_int, str: [*c]const u8) void {
+    std.log.err("GLFW Error '{}'': {s}", .{ errn, str });
+}
+
+pub fn main() !void {
+    c.glfwSetErrorCallback(errorCallback);
+
+    if (!c.glfwInit()) {
+        return;
+    }
+    defer c.glfwTerminate();
+
+    const GLSL_VERSION = "#version 130";
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    const window = c.glfwCreateWindow(600, 800, "example_glfw_opengl3", null, null);
+    if (window == null) {
+        return;
+    }
+    defer c.glfwDestroyWindow(window);
+
+    c.glfwMakeContextCurrent(window);
+    c.glfwSwapInterval(1);
+
+    c.CIMGUI_CHECKVERSION();
+    c.ImGui_CreateContext(null);
+    defer c.ImGui_DestroyContext(null);
+
+    const imio = c.ImGui_GetIO();
+    imio.*.ConfigFlags = c.ImGuiConfigFlags_NavEnableKeyboard;
+
+    c.ImGui_StyleColorsDark(null);
+
+    c.cImGui_ImplGlfw_InitForOpenGL(window, true);
+    defer c.cImGui_ImplGlfw_Shutdown();
+
+    c.cImGui_ImplOpenGL3_InitEx(GLSL_VERSION);
+    defer c.cImGui_ImplOpenGL3_Shutdown();
+
+    while (!c.glfwWindowShouldClose(window)) {
+        c.glfwPollEvents();
+
+        c.cImGui_ImplOpenGL3_NewFrame();
+        c.cImGui_ImplGlfw_NewFrame();
+        c.ImGui_NewFrame();
+
+        c.ImGui_ShowDemoWindow();
+
+        c.ImGui_Render();
+
+        var width: c_int = 0;
+        var height: c_int = 0;
+        c.glfwGetFramebufferSize(window, &width, &height);
+        c.glViewport(0, 0, width, height);
+        c.glClearColor(0.0, 0.0, 0.0, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+        c.cImGui_ImplOpenGL3_RenderDrawData(c.ImGui_GetDrawData());
+
+        c.glfwSwapBuffers(window);
+    }
+}
