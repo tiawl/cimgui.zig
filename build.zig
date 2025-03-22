7365ed9673fde589dfd3a04e61fc5919a63e7782
tiawl <pabtomas@protonmail.com>
2025-03-22 23:03:11 +0100

    remove update option

--- build.zig
+++ build.zig
@@ -169,9 +169,7 @@ pub fn build(builder: *std.Build) !void {
 
     const path = try Paths.init();
 
-    if (toolbox.instance().ptrBuilder().option(bool, "update", "Update binding") orelse false) {
-        try update(&path, &dependencies);
-    }
+    if (toolbox.instance().getUpdate()) try update(&path, &dependencies);
 
     const lib = toolbox.instance().ptrBuilder().addStaticLibrary(.{
         .name = "cimgui",
